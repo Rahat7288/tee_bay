965b0ba427572854b3618ea972189416585ecc27
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-03 12:13:06 +0600

    appColor class for theme data with color codes

---
+++ lib/core/app_theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color backgroundColor = Color(0XFFFBFBFB);
+  static const Color progressColor = Color(0XFFC4FB6D);
+  static const Color inactiveColor = Color(0XFFB3C8CF);
+  static const Color activeBorder = Color(0XFFF3CCFF);
+  static const Color buttonColor1 = Color(0XFFA555EC);
+  static const Color buttonColor2 = Color(0XFFD09CFA);
+  static const Color buttonColor3 = Color(0XFFFF6464);
+  static const Color titleText = Color(0XFF222831);
+  static const Color subTitleText = Color(0XFF393E46);
+  static const Color contentText = Color(0XFF00263B);
+  static const Color buttonText = Color(0XFFFFFFFF);
+}
